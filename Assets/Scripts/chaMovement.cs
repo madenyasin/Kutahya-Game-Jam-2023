aed37700ddd81ac0db140318441c308ca116d6bd
samet <basabdussamed@gmail.com>
2023-10-07 01:38:40 +0300

    ziplama eklendi

--- Assets/Scripts/chaMovement.cs
+++ Assets/Scripts/chaMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class chaMovement : MonoBehaviour
 {
-    
+    private bool inputEnabled = true;
+    private bool isJumping = false; // jumping animasyonunun oynadığını takip etmek için bir değişken
     public float maxWalkSpeed;
     public float jumpSpeed;
     Vector3 playerVelocity;
@@ -19,16 +20,49 @@ void Start() {
     
 }
 
-void Update() {
-    playerVelocity.x = Input.GetAxis("Horizontal") * maxWalkSpeed;
-    animator.Play("WalkingRight");
-    if (Input.GetKeyDown(KeyCode.Space)) {
-        playerVelocity.y = jumpSpeed;
-    } else {
-        playerVelocity.y = rb.velocity.y;
-    }
-    rb.velocity = playerVelocity;
 
+
+void Update()
+{
+    if(inputEnabled==true){
+        if (Input.GetAxis("Horizontal") != 0)
+            {
+                playerVelocity.x = Input.GetAxis("Horizontal") * maxWalkSpeed;
+                animator.Play("WalkingRight");
+            }
+            else
+            {
+                playerVelocity.x = 0; // Hareketsizlik durumunda x hızını sıfırla
+                if (!isJumping) // jumping animasyonu oynanmıyorsa Idle animasyonunu oynat
+                {
+                    animator.Play("Idle");
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerVelocity.y = jumpSpeed;
+                animator.Play("Jumping");
+                isJumping = true;
+                inputEnabled=false; // jumping animasyonunun oynadığını işaretle
+            }
+            else
+            {
+                playerVelocity.y = rb.velocity.y;
+            }
+
+            rb.velocity = playerVelocity;
+            }
+    
+}
+
+// Jumping animasyonunun bitişinde çağrılacak olan bir fonksiyon
+
+public void OnJumpAnimationEnd()
+{
+    isJumping = false;
+    inputEnabled = true;
+    
 }
 
 }
