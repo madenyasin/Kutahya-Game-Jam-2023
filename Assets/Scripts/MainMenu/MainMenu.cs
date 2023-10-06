f496b059c028281db6bcdc3442eb00a5aebc8441
samet <basabdussamed@gmail.com>
2023-10-07 01:54:27 +0300

    version 1

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public void playGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("game");
     }
     public void quitGame()
     {
